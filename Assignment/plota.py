a8999b5e351257464285f869097d85d428565041
arghya dutta <arghyaduttabku@gmail.com>
2022-02-09 19:22:24 +0530

    change assignment

--- Assignment/plota.py
+++ Assignment/plota.py
@@ -11,9 +11,11 @@ x2=np.cos(3*t)
 y2=np.sin(2*t)
 plt.plot(x2,y2, label="b", c='r' )
 
+
 x3=np.abs(np.cos(4*t))*np.cos(t)
 y3=np.abs(np.cos(4*t))*np.sin(t)
 plt.plot(x3,y3, label="c",c= 'k' )
+
 plt.legend()
 plt.show()
 
